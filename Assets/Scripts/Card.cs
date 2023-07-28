785a3eab3e2d59946c68ba455c490de5b24605a2
MatthewDemoe <mattdemoe@gmail.com>
2023-07-27 23:18:54 -0400

    Initial Commit.
    Add card class that contains card information & CardListRetriever that will fetch cube list from cubecobra & fetch card images from scryfall.

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,54 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Networking;
+
+[System.Serializable]
+public class Card
+{
+    private UnityWebRequest www;
+
+    public string sCardName { get; private set; } = string.Empty;
+    public string sImageUri { get; private set; } = string.Empty;
+
+    public string sCardLayout { get; private set; } = string.Empty;
+
+    public Texture cardImage { get; private set; }
+
+    public bool bIsFinishedLoading { get; private set; } = false;
+
+    public Card(JToken cardToken)
+    {
+        sCardName = cardToken["name"].ToString();
+        Debug.Log(cardToken);
+
+        sCardLayout = cardToken["layout"].ToString();
+
+        if (sCardLayout.Equals("transform"))
+            sImageUri = cardToken["card_faces"][0]["image_uris"]["png"].ToString();
+        
+        else
+            sImageUri = cardToken["image_uris"]["png"].ToString();
+
+        InitializeCard();
+    }
+
+    private async void InitializeCard()
+    {
+        www = UnityWebRequestTexture.GetTexture(sImageUri);
+        www.SendWebRequest();
+
+        while (!www.isDone)
+            await Task.Delay(1000 / 30);
+
+        if (UnityWebRequest.Result.ConnectionError == www.result || UnityWebRequest.Result.ProtocolError == www.result)
+        {
+            Debug.Log(www.error);
+        }
+
+        cardImage = ((DownloadHandlerTexture)www.downloadHandler).texture;
+        bIsFinishedLoading = true;
+    }
+}
---
+++ Assets/Scripts/CubeListRetriever.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+using Newtonsoft.Json.Linq;
+using System.Linq;
+using Newtonsoft.Json.Serialization;
+using UnityEngine.UI;
+
+public class CubeListRetriever : MonoBehaviour
+{
+    [SerializeField]
+    string[] cardNames;
+
+    [SerializeField]
+    RawImage cardImage;
+
+    Card[] cardList;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(GetCardList());
+    }
+
+    IEnumerator GetCardList()
+    {
+        UnityWebRequest www = UnityWebRequest.Get("https://cubecobra.com/cube/api/cubelist/alh");
+
+        yield return www.SendWebRequest();
+
+        if (UnityWebRequest.Result.ConnectionError == www.result || UnityWebRequest.Result.ProtocolError == www.result)
+        {
+            Debug.Log(www.error);
+            StopAllCoroutines();
+        }
+
+        string listString = www.downloadHandler.text;
+        cardNames = listString.Split('\n');
+
+        www.Dispose();
+        www = null;
+
+        StartCoroutine(GetCardImageList());
+    }
+
+    IEnumerator GetCardImageList()
+    {
+        UnityWebRequest www;
+        JObject scryfallData;
+
+        cardList = new Card[cardNames.Length];
+
+        for (int cardIndex = 0; cardIndex < 10; cardIndex++)
+        {
+            www = UnityWebRequest.Get("https://api.scryfall.com/cards/search?q=" + cardNames[cardIndex] + "&pretty=true");
+            yield return www.SendWebRequest();
+
+            if (UnityWebRequest.Result.ConnectionError == www.result || UnityWebRequest.Result.ProtocolError == www.result)
+            {
+                Debug.Log(www.error);
+                break;
+            }
+
+            scryfallData = JObject.Parse(www.downloadHandler.text);
+            cardList[cardIndex] = new Card(scryfallData["data"][0]);
+
+            yield return new WaitUntil(() => cardList[cardIndex].bIsFinishedLoading);
+            cardImage.texture = cardList[cardIndex].cardImage;
+            cardImage.SetNativeSize();
+            cardImage.GetComponent<RectTransform>().localScale = Vector3.one * 0.5f;
+        }
+    }
+}
